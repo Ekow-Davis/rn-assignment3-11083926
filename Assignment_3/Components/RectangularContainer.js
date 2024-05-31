0a36da2ea47c453e0be375b973bc99084048c9ea
Ekow-Davis <paakowdavis@gmail.com>
2024-05-31 18:03:34 +0000

    Added second component

---
+++ Assignment_3/Components/RectangularContainer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+
+const RectangularContainer = ({ label }) => {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.label}>{label}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      width: 300,  // Width of the square container
+      height: 150, // Height of the square container
+      backgroundColor: '#ffffff', // White background for the square container
+      justifyContent: 'center',
+      alignItems: 'center',
+      elevation: 5, // Optional: for shadow effect on Android
+      borderRadius: 16,
+      margin: 8,
+    },
+  
+    label: {
+      position: 'absolute',
+      top: 20,
+      left: 10,
+      backgroundColor: 'rgba(255, 255, 255, 0.7)', // Semi-transparent background for readability
+      paddingHorizontal: 5,
+      paddingVertical: 2,
+      borderRadius: 3,
+      fontSize: 14,
+      fontWeight: 'bold',
+      color: '#333', // Text color
+    }
+
+});
+
+export default RectangularContainer;
