3b85c6e2a0776dd94372508ed7b6a84b7fe884a4
Ekow-Davis <paakowdavis@gmail.com>
2024-06-01 14:28:21 +0000

    Added search image, made styling changes, updated SquarContainer to render images

--- Assignment_3/App.js
+++ Assignment_3/App.js
@@ -3,7 +3,7 @@ import { SafeAreaView, StyleSheet, ScrollView, Image, TextInput, Text, View, Fla
 import SquareContainer from './Components/SquareContainer';
 import RectangularContainer from './Components/RectangularContainer'
 
-const horizontalData = [
+const verticalData = [
   { key: '1', label: 'Mobile App Dev' },
   { key: '2', label: 'Web Dev' },
   { key: '3', label: 'Creative Dev' },
@@ -11,8 +11,18 @@ const horizontalData = [
   { key: '5', label: 'DS' },
 ];
 
+const horizontalData = [
+  { key: '1', imageUrl: require('./assets/Images/CrossLeg.png'), label: 'Exercise', minilabel: '12 Tasks' },
+  { key: '2', imageUrl: require('./assets/Images/DeskStudy.png'), label: 'Study', minilabel: '12 Tasks' },
+  { key: '3', imageUrl: require('./assets/Images/CrossLeg.png'), label: 'Cook', minilabel: '12 Tasks' },
+  { key: '4', imageUrl: require('./assets/Images/DeskStudy.png'), label: 'Code', minilabel: '12 Tasks' },
+  { key: '5', imageUrl: require('./assets/Images/CrossLeg.png'), label: 'Teach', minilabel: '12 Tasks' },
+  { key: '6', imageUrl: require('./assets/Images/DeskStudy.png'), label: 'Party', minilabel: '12 Tasks' },
+];
+
 const ProfileImage = require("./assets/Images/ProfilePicture.png");
 const FilterImage = require("./assets/Images/FilterIcon.png");
+const SearchImage = require("./assets/Images/SearchIcon.png");
 
 
 
@@ -25,48 +35,43 @@ const App =() => {
     <View style={styles.container}>
     <StatusBar style="auto" />
     <ScrollView>
+
     <View style={{marginBottom: 20, marginTop: 50, flexDirection: 'row',}}>
       <View>
       <Text style={{fontSize: 28, fontWeight: 'bold', marginLeft: 40,}}>Hello, Devs</Text>
       <Text style={{fontsize: 8, marginLeft: 40,}}>14 tasks today</Text>
       </View>    
-      <Image source={ProfileImage} style={{borderRadius: 50, marginVertical: 10, width: 60, height: 60, backgroundColor: "white", marginLeft: 100,}} />
+      <Image source={ProfileImage} style={{borderRadius: 50, marginVertical: 10, width: 60, height: 60, backgroundColor: "white", marginLeft: 135,}} />
     </View>
-    <View style={{flexDirection: 'row',}}>
-      <TextInput style={{backgroundColor: "white", width: 250, height: 45, borderRadius: 10, marginLeft: 40, }} />
+
+    <View style={{flexDirection: 'row', marginLeft: 10,}}>
+      <View style={{backgroundColor: "white", width: 280, height: 45, marginLeft: 10, borderRadius: 10,}}>
+        <Image source={SearchImage} style={{width: 30, height: 30, top: 10, marginLeft: 10, marginRight: 5,}}/>
+      <TextInput placeholder='Search' placeholderTextColor='#999' style={{backgroundColor: "white", width: 150, height: 40, borderRadius: 10, marginLeft: 40, top: -27, }} />
+      </View>
       <Image source={FilterImage} style={{height: 50, width: 50, marginLeft: 20, }} />
     </View>
+
     <Text style={{fontSize: 24, fontWeight: 'bold', marginLeft: 40, marginTop: 20, marginBottom: 20,}}>Categories</Text>
-    <ScrollView horizontal={true} contentContainerStyle={{alignItems: 'center', paddingLeft: 20, paddingHorizontal: 10, flexDirection: 'row',}}>
-        <SquareContainer
-         imageUrl="./assets/Images/CrossLeg.png"
-          label="Excercise"
-          minilabel="12 Tasks" />
-        <SquareContainer
-         imageUrl="./assets/Images/DeskStudy.png"
-          label="Study"
-          minilabel="12 Tasks" />
-        <SquareContainer
-         imageUrl="./assets/Images/CrossLeg.png"
-          label="Cook"
-          minilabel="12 Tasks" />
-        <SquareContainer
-         imageUrl="./assets/Images/DeskStudy.png"
-          label="Code"
-          minilabel="12 Tasks" />
-        <SquareContainer
-         imageUrl="./assets/Images/CrossLeg.png"
-          label="Teach"
-          minilabel="12 Tasks" />
-        <SquareContainer
-         imageUrl="./assets/Images/DeskStudy.png"
-          label="Party"
-          minilabel="12 Tasks" />
-      </ScrollView>
+    <FlatList
+          horizontal
+          data={horizontalData}
+          renderItem={({ item }) => (
+            <SquareContainer
+              imageUrl={item.imageUrl}
+              label={item.label}
+              minilabel={item.minilabel}
+            />
+          )}
+          keyExtractor={(item) => item.key}
+          contentContainerStyle={styles.horizontalList}
+          showsHorizontalScrollIndicator={false}
+        />
+
       <View>
         <Text style={{fontSize: 24, fontWeight: 'bold', marginLeft: 40, marginTop: 20, marginBottom: 10,}}>Ongoing Task</Text>
         <FlatList
-            data={horizontalData}
+            data={verticalData}
             renderItem={({ item }) => (
               <RectangularContainer
                 label={item.label}
@@ -76,6 +81,7 @@ const App =() => {
             style={{flexGrow: 0, marginLeft: 10,}}
           />
       </View>
+      
     </ScrollView>
     </View>
     </SafeAreaView>
@@ -88,6 +94,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f7f0e8',
   },
+
+  horizontalList: {
+    alignItems: 'center',
+    paddingLeft: 20,
+    marginLeft: 5,
+  },
+
 });
 
 export default App;
